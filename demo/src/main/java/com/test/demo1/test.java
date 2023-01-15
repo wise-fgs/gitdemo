6e72bea6c73cc1b1c8558271bacfcb09e16a737e
fgs <1359706495@qq.com>
2023-01-15 12:39:42 +0800

    second first

--- demo/src/main/java/com/test/demo1/test.java
+++ demo/src/main/java/com/test/demo1/test.java
@@ -3,5 +3,6 @@ package com.test.demo1;
 public class test {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
