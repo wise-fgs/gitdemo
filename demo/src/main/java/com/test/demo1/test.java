bd05ea51886ef5d867d4b5602fe162150c152f11
fgs <1359706495@qq.com>
2023-01-15 12:40:50 +0800

    hot-fix commit

--- demo/src/main/java/com/test/demo1/test.java
+++ demo/src/main/java/com/test/demo1/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("hot-fix commit");
     }
 }
